dbe758af4210386ea89a0c568b5d65d70cb85c59
DevBazz <bazaniqbal@gmail.com>
2026-02-04 21:45:12 +0500

    sidebar footer

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Sidebar, SidebarContent, SidebarGroup, SidebarHeader, SidebarMenu } from './ui/sidebar'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarMenu } from './ui/sidebar'
 import { BellIcon, HomeIcon, NotebookIcon, UserIcon } from 'lucide-react'
 import ToggleTheme from './toggle-theme'
 import { Separator } from './ui/separator'
 import Link from 'next/link'
+import { SignIn, UserButton } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
-const AppSidebar = () => {
+const AppSidebar = async () => {
+
+    const user  = await auth()
 
 const sidebarItems = [
     { name: 'Home',
@@ -39,10 +44,10 @@ const sidebarItems = [
         <SidebarContent>
             <SidebarGroup>
                 <SidebarMenu>
-                    <ul className='flex items-center justify-center flex-col space-y-4 mt-4'>
+                    <ul className='flex items-center justify-center flex-col space-y-4 mt-4 gap-2'>
                         {sidebarItems.map((item) => (
                             <li key={item.name} className='w-full'>
-                                <Link href={item.href} className='text-lg flex items-center font-semibold hover:text-gray-400'>
+                                <Link href={item.href} className='text-lg flex gap-2 items-center  hover:text-gray-400'>
                                     {item.icon}
                                     {item.name}
                                 </Link>
@@ -52,6 +57,19 @@ const sidebarItems = [
                 </SidebarMenu>
             </SidebarGroup>
         </SidebarContent>
+
+        <SidebarFooter className='border-2 rounded-lg mx-2 my-2 h-17 w-60 '>
+            <div className='mb-4 mt-2 flex gap-2 items-center'>
+              <Avatar className='h-full'>
+                <AvatarImage src='/next.svg'/>
+                <AvatarFallback>BI</AvatarFallback>
+              </Avatar>
+              <div>
+                <h3 className='text-sm'>Bazan Iqbal</h3>
+                <p className='text-sm text-gray-500'>bazan@example.com</p>
+              </div>  
+            </div>
+        </SidebarFooter>
     </Sidebar>
   )
 }
